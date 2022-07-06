ceb3c2c364947c34e37afac5b72940cd3ee8eaff
maximeDzN <maxime.dazin@gmail.com>
2022-07-06 14:17:48 +0200

    student create and update

--- src/app/pages/student-form-page/container/student-form-page/student-form-page.component.ts
+++ src/app/pages/student-form-page/container/student-form-page/student-form-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Student } from 'src/app/modules/student/models/student';
+import { StudentService } from 'src/app/modules/student/services/student.service';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'div[app-student-form-page]',
@@ -9,15 +13,65 @@ import { Router } from '@angular/router';
 export class StudentFormPageComponent implements OnInit {
 
   @HostBinding('class') class = 'frame frame--height frame--top frame--padd';
+  id!:number;
+  student!:Student;
+  studentForm!: FormGroup;
+  submitted:boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private route: ActivatedRoute,private studentService: StudentService,private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.studentForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      mail: ['', Validators.required],
+      address: ['', Validators.required],
+    });
+
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      if(this.id){
+        this.studentService.get(this.id).subscribe((stu) => {
+          this.student = stu;
+          this.studentForm.get("firstName")!.setValue(stu.firstName);
+          this.studentForm.get("lastName")!.setValue(stu.lastName);
+          this.studentForm.get("birthDate")!.setValue((new Date(stu.birthDate)).toISOString().split('T')[0]);
+          this.studentForm.get("phoneNumber")!.setValue(stu.phoneNumber);
+          this.studentForm.get("mail")!.setValue(stu.mail);
+          this.studentForm.get("address")!.setValue(stu.address);
+        });
+      }
+     
+    });
+ 
   }
 
   openStudentInfo() {
-    this.router.navigateByUrl('/students/1');
-  };
+    this.router.navigateByUrl(`/students/${this.id}`);
+  }
+
+  onSubmit(){
+    if(this.studentForm.valid){
+      let student:Student = this.studentForm.value;
+      if(this.id){
+        student.id = this.id;
+      }
+      this.studentService.create(student).subscribe((stu)=> {
+        if(this.id){
+          this.router.navigate([`/students/${this.id}`]);
+        } else {
+          this.router.navigate(["/students"]);
+        }
+        
+      });
+    } else {
+
+    }
+    
+    
+  }
 
 
 }
--- src/app/pages/student-form-page/student-form-page.module.ts
+++ src/app/pages/student-form-page/student-form-page.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StudentFormPageComponent } from './container/student-form-page/student-form-page.component';
 
 
@@ -9,7 +10,9 @@ import { StudentFormPageComponent } from './container/student-form-page/student-
     StudentFormPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class StudentFormPageModule { }
